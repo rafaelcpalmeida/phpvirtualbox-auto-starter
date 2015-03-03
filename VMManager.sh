4d72c24b2ed1455b77483155d5bc01ce6be36f13
Rafael Almeida <rafael.almeida@outlinksoftware.com>
2015-03-03 11:00:53 +0000

    Code optimization

--- VMManager.sh
+++ VMManager.sh
@@ -1,33 +1,32 @@
 #! /bin/sh
 # /etc/init.d/VMManager
 
-#Edit these variables!
 VMUSER=vbox
-VM=( VM_Name )
+declare -a VM=('VirtualBox-Name');
 
 case "$1" in
-	start)
-		echo "Starting VirtualBox VM..."
-		cd
-		echo "Available VM's ${VM[@]}"
-		for i in "${VM[@]}"
-		do
-			sudo -H -b -u $VMUSER /usr/bin/VBoxManage startvm "$i" --type headless
-		done
-    		sleep 3
-    	;;
-  	stop)
-   		echo "Saving state of Virtualbox VM..."
-		for i in "${VM[@]}"
+        start)
+                echo "Starting VirtualBox VM..."
+                cd
+                echo "Available VM's: ${VM[@]}"
+                for i in "${VM[@]}"
                 do
-			sudo -H -u $VMUSER /usr/bin/VBoxManage controlvm "$i" savestate
-    		done
-		sleep 3
-    	;;
-  	*)
-    		echo "Usage: /etc/init.d/StartVM {start|stop}"
-    		exit 1
-    	;;
+                        sudo -H -b -u $VMUSER /usr/bin/VBoxManage startvm "$i" --type headless
+                done
+                sleep 3
+        ;;
+        stop)
+                echo "Saving state of Virtualbox VM..."
+                for i in "${VM[@]}"
+                do
+                        sudo -H -u $VMUSER /usr/bin/VBoxManage controlvm "$i" savestate
+                done
+                sleep 3
+        ;;
+        *)
+                echo "Usage: /etc/init.d/StartVM {start|stop}"
+                exit 1
+        ;;
 esac
 
 exit 0
