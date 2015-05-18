d2bf9c7b4dadeec15b7a77d024601c9f0e89432e
Rafael Almeida <rafaelcpalmeida@users.noreply.github.com>
2015-05-18 13:53:33 +0100

    Update VMBacker.sh

--- VMBacker.sh
+++ VMBacker.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-# /etc/init.d/VMBacker
+# /etc/init.d/VMBacker.sh
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
 #Change vbox to your virtualbox user
 VMUSER=vbox
-#Insert your virtual machines in here. The pattern is 'VM Name'*space*'VM Name', like this 'VM1' 'VM2' 'VM3'
-declare -a VM=('VirtualBox-Name');
+#Insert your virtual machines in here. The pattern is 'VM Name'space'VM Name', like this 'VM1' 'VM2' 'VM3'
+declare -a VM=('VirtualBox-Name')
 
 now=$(date +'%d/%m/%Y')
 
@@ -15,12 +16,12 @@ case "$1" in
                 echo "Available VM's: ${VM[@]}"
                 for i in "${VM[@]}"
                 do
-                        sudo -H -b -u $VMUSER /usr/bin/VBoxManage snapshot "$i" take "$now"
+                       su $VMUSER /usr/bin/VBoxManage snapshot "$i" take "$now"
                 done
                 sleep 3
         ;;
         *)
-                echo "Usage: /etc/init.d/VMBacker {back}"
+                echo "Usage: /etc/init.d/VMBacker.sh {back}"
                 exit 1
         ;;
 esac
